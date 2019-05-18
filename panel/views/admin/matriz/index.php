0ef407f6cc0e03720672b3bb16cff50a2b9fe054
leonardo_mauricio <30731660+leomauricio7@users.noreply.github.com>
2019-05-17 23:36:19 -0300

    Add files via upload

--- panel/views/admin/matriz/index.php
+++ panel/views/admin/matriz/index.php
@@ -13,3 +13,20 @@
         <?php require_once 'arvore.php' ?>
     </div> <!-- / .card-body -->
 </div>
+<div class="card">
+    <div class="card-header">
+        <div class="row align-items-center">
+            <div class="col"> 
+            <!-- Title -->
+            <h4 class="card-header-title">
+                Ganhos e bonificação
+            </h4>
+            </div>
+        </div> <!-- / .row -->
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <?php require_once 'legenda.php' ?>
+        </div>
+    </div> <!-- / .card-body -->
+</div>
---
+++ panel/views/admin/matriz/legenda.php
@@ -0,0 +1,49 @@
+<!-- legendas -->
+<table class="table table-nowrap table-bordered" style="text-align: center;font-weight: bold;">
+    <thead>
+        <tr>
+            <th scope="row">NÍVEL</th>
+            <td>1º</td>
+            <td>2º</td>
+            <td>3º</td>
+            <td>4º</td>
+            <td>5º</td>
+            <td>6º</td>
+            <td>7º</td>
+            <td>8º</td>
+        </tr>
+        <tr>
+            <th scope="row">AFILIADOS</th>
+            <td>2</td>
+            <td>4</td>
+            <td>8</td>
+            <td>16</td>
+            <td>32</td>
+            <td>64</td>
+            <td>128</td>
+            <td>256</td>
+        </tr>
+        <tr>
+            <th scope="row">VALOR INDIVIDUAL</th>
+            <td>QUALIFICADORES</td>
+            <td>R$ 5,00</td>
+            <td>R$ 5,00</td>
+            <td>R$ 5,00</td>
+            <td>R$ 5,00</td>
+            <td>R$ 5,00</td>
+            <td>R$ 5,00</td>
+            <td>R$ 15,00</td>
+        </tr>
+        <tr>
+            <th scope="row">VALOR TOTAL</th>
+            <td>QUALIFICADORES</td>
+            <td>R$ 20,00</td>
+            <td>R$ 40,00</td>
+            <td>R$ 80,00</td>
+            <td>R$ 160,00</td>
+            <td>R$ 320,00</td>
+            <td>R$ 640,00</td>
+            <td>R$ 3840,00</td>
+        </tr>
+    </thead>
+</table>
